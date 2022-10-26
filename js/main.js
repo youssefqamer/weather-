8868a40412c9244901d9f193d653d99d7c01e223
youssefqamer <youssefqamer69@gmail.com>
2022-10-26 09:14:29 +0200

    add rain probability

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const city=document.querySelector('.city')
 const nCity=document.querySelectorAll('.n-city')
 const tDegree=document.querySelector('.t-degree')
 const tStatus=document.querySelector('.t-status')
+const tRain=document.querySelector('.t-rain')
 let tIcon=document.getElementById('t-icon')
 const tHumadity=document.querySelector('.humidaty')
 const wSpeed=document.querySelector('.w-speed')
@@ -14,17 +15,21 @@ const nDate=document.querySelectorAll('.n-date')
 const nDegree=document.querySelectorAll('.n-degree')
 const nIcon=document.querySelectorAll('.n-icon')
 const nstatus=document.querySelectorAll('.n-status')
+const nRain=document.querySelectorAll('.n-rain')
 const nHumadity=document.querySelectorAll('.n-humidaty')
 const nWSpeed=document.querySelectorAll('.n-w-speed')
 const nWDirection=document.querySelectorAll('.n-w-direction')
+
 let result;
 let date;
 const searchInput=document.querySelector('.search')
 const array=['Sunday','Monday','Tuesday','Wednesday','Thursday','Fridday','Saturday']
 // 
-async function getData(searchCity='london'){
+async function getData(searchCity='cairo'){
     let response=await fetch(`https://api.weatherapi.com/v1/forecast.json?key=641fa38da4624675bf982728221410&q=${searchCity}&days=3`)
-     result=await response.json() 
+    //  let response=await fetch(`https://api.weatherapi.com/v1/current.json?key=d5fea27251824d0ea0564341222410&q=${searchCity}&days=3`)
+    
+    result=await response.json() 
      showToday()
     showNextDays()
 }
@@ -35,7 +40,9 @@ tDay.innerHTML=array[date.getDay()];
 tDate.innerHTML=result.forecast.forecastday[0].date;
 city.innerHTML=result.location.name;
 tDegree.innerHTML= `<sub >${result.current.temp_c} oC</sub>`;
-tStatus.innerHTML=result.current.condition.text
+tStatus.innerHTML=result.current.condition.text;
+tRain.innerHTML=`rain probability: ${result.forecast.forecastday[0].day.daily_chance_of_rain} %`;
+
 tIcon.setAttribute("src",`https:${result.current.condition.icon}`)
 tHumadity.innerHTML=`${result.current.humidity} %`
 wSpeed.innerHTML=`${result.current.wind_kph}Km`
@@ -49,6 +56,7 @@ function showNextDays(){
     nDegree[i].innerHTML=`<sub >${result.forecast.forecastday[i+1].day.maxtemp_c} oC</sub>`;
     nIcon[i].setAttribute("src",`https:${result.forecast.forecastday[i+1].day.condition.icon}`)
     nstatus[i].innerHTML=result.forecast.forecastday[i+1].day.condition.text;
+    nRain[i].innerHTML=` rain probability: ${result.forecast.forecastday[i+1].day.daily_chance_of_rain}%`;
     nHumadity[i].innerHTML=`${result.forecast.forecastday[i+1].day.avghumidity} %`;
     nWSpeed[i].innerHTML=`${result.forecast.forecastday[i+1].day.maxwind_kph} Km`;
     nWDirection[i].innerHTML=`${result.forecast.forecastday[i+1].hour[0].wind_dir}`;
